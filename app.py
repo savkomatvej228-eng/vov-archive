f0978b2295473da35559a1b5bc3c970c89e8289e
savkomatvej228-eng <savkomatvej228@gmail.com>
2026-03-19 20:53:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -113,5 +113,7 @@ def custom_static(filename):
 
 # ----- ЗАПУСК -----
 if __name__ == '__main__':
-    threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:5000')).start()
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    
+    port = int(os.environ.get("PORT", 5000)
+               
+    app.run(host='0.0.0.0', port=port)
